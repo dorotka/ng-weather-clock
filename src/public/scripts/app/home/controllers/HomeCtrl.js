b11119a6dffb28007fa9c7ea02cc1e1c5e259f27
Dorotka <dorota_oleszczuk@yahoo.com>
2015-08-04 00:40:59 -0400

    Added call for weather and some styling changes

--- src/public/scripts/app/home/controllers/HomeCtrl.js
+++ src/public/scripts/app/home/controllers/HomeCtrl.js
@@ -6,8 +6,8 @@ function HomeCtrl($scope, ForecastService, $timeout, $interval) {
 		$scope.loading = true;
 		$scope.getCurrentLocalTime();
 		$scope.getDay();
-		// units can be 'us' for F or 'si' for C
-		$scope.settings = 'us';
+		// units can be 'us' for F or 'si'/'ca' for C
+		$scope.unit = 'ca';
 		// can be 'night' or 'day'
 		$scope.timeOfDay;
 		$scope.getTimeOfDay();
@@ -17,12 +17,14 @@ function HomeCtrl($scope, ForecastService, $timeout, $interval) {
 		$scope.getLocation();
 	};
 
-	$scope.assignIcon = function(){
+	$scope.assignIcon = function(icon){
 		//TODO: Use after weather check and assign depending on weather
 		/* Icons correspond to those: clear-day, clear-night, rain, snow, sleet, wind, fog, 
 		cloudy, partly-cloudy-day, or partly-cloudy-night
 		*/
 		$scope.timeOfDay == 'night' ? $scope.icon = 'fa-moon-o' : $scope.icon = 'fa-sun-o';
+		//$scope.icon = icon;
+		//$scope.iconSrc = '/media/images/weather-icons/' + icon + '.svg';
 	};
 
 	$scope.getCurrentLocalTime = function(){
@@ -30,6 +32,7 @@ function HomeCtrl($scope, ForecastService, $timeout, $interval) {
 		var tickInterval = 1000;
 		
 		var tick = function(){
+			// once a full minute, change interval to 60000ms
 			if(tickInterval == 1000 && moment().second() == 0){
 				$interval.cancel($scope.stop);
 				tickInterval = 60000;
@@ -63,7 +66,7 @@ function HomeCtrl($scope, ForecastService, $timeout, $interval) {
         	$scope.lon = position.coords.longitude;
 
 			$scope.getCityName($scope.lat, $scope.lon);
-	        $scope.getWeather($scope.lat, $scope.lon);
+	        $scope.getWeather($scope.lat, $scope.lon, $scope.unit);
         }
 
         function showErr (response){
@@ -71,31 +74,43 @@ function HomeCtrl($scope, ForecastService, $timeout, $interval) {
         }
     };
 
-    $scope.getWeather = function(lat, lon){
+    $scope.getWeather = function(lat, lon, unit){
         console.log("lat, lon", lat, lon);
 
-        // ForecastService.Conditions.get({latitude : lat, longitude : lon, settings : $scope.settings}, function(response){
-        // 	test(response);
-        //     console.log("Forcast response", response);
-        // });
-		ForecastService.MyConditions.get({})
+        function setConditions(data){
+            console.log("Weather", data );
+            $scope.currently = data.currently;
+            console.log("$scope.currently", $scope.currently, $scope.currently.temperature);
+            $scope.daily = data.daily;
+            $scope.hourly = data.hourly;
+            $scope.weatherTimezone = data.timezone;
+            $scope.alerts = data.alerts;
+            $scope.weatherTime = $scope.currently.time;
+            $scope.assignIcon($scope.currently.icon);
+            // console.log("time we are checking the weather for", moment.unix($scope.weatherTime).format('YYYY-MM-DDTHH:mm:ss'), 
+            // moment.unix($scope.weatherTime).format('hh:mm A'));
+        }
+
+        //ForecastService.getWeather(setConditions, lat,  lon, unit);
+
+        // get static weather data
+        ForecastService.getFakeWeather().get()
 		.$promise.then(
 		    //success
-		    function( response ){/*Do something with value*/
-		    	//console.log("Forcast response", response);
-		    	$scope.conditions = response.currently;
+		    function( response ){
+		    	console.log("Forcast response", response);
+		    	$scope.currently = response.currently;
+		    	$scope.assignIcon($scope.currently.icon);
+		    	$scope.loading = false;
 		    },
 		    //error
 		    function( error	 ){
 		    	console.log("Forcast error", response);
-		    	/*Do something with error*/
-
+		    	$scope.loading = false;
 		    }
 		);
 
-		//$scope.assignIcon();
-
-		};
+	};
 
     $scope.initGeocoder = function(){
         if(!$scope.geocoder) $scope.geocoder = new google.maps.Geocoder();
@@ -108,9 +123,6 @@ function HomeCtrl($scope, ForecastService, $timeout, $interval) {
             if (status == google.maps.GeocoderStatus.OK) {
             //console.log("result", results);
 			    if (results[1]) {
-			         //formatted address
-			        //console.log("address", results[0].formatted_address);
-			        //find country name
 			        for (var i=0; i<results[0].address_components.length; i++) {
 			            for (var b=0;b<results[0].address_components[i].types.length;b++) {
 
--- src/public/scripts/app/home/services/forecastService.js
+++ src/public/scripts/app/home/services/forecastService.js
@@ -9,13 +9,34 @@ var home = angular.module('home');
 *
 *
 */
-function ForecastService($resource, call) {
-    console.log("service");
-    return {
+function ForecastService($resource, $http) {
 
-        Conditions: $resource('https://api.forecast.io/forecast/d5c4f7fbc68f86a1658a5a74dfcd082f/:latitude,:longitude?units=[:settings]', {latitude : '@latitude', longitude : '@longitude', settings : '@settings'}), 
-        MyConditions: $resource('/scripts/app/nyforcast.json', {})
+	function getWeather (JSON_CALLBACK, LAT, LON, UNIT) {
+        $http.jsonp('https://api.forecast.io/forecast/d5c4f7fbc68f86a1658a5a74dfcd082f/' + LAT + ',' + LON + '?callback=JSON_CALLBACK&units=' + UNIT)
+          .success(function(data){
+            //console.log("data", data);
+            JSON_CALLBACK(data);
+          })
+          .error(function(data, err){
+            console.log('Error retrieving forecast', err, data);
+          });
+    }
+
+    function getFakeWeather(){
+    	return $resource('/scripts/app/nyforcast.json', {});
+    }
+      
+    return {
+        getWeather: getWeather,
+        getFakeWeather : getFakeWeather
     };
+    // console.log("service");
+    // return {
+
+    //     Conditions: $resource('https://api.forecast.io/forecast/d5c4f7fbc68f86a1658a5a74dfcd082f/:latitude,:longitude?units=[:settings]', {latitude : '@latitude', longitude : '@longitude', settings : '@settings'}), 
+    //     MyConditions: $resource('/scripts/app/nyforcast.json', {})
+    // };
+
 }
 
-home.factory('ForecastService', ['$resource', ForecastService]);
+home.factory('ForecastService', ['$resource', '$http', ForecastService]);
